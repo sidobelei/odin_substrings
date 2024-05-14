56090c2029f817992d1e56e66099ff7e1c579f84
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-14 15:01:22 -0700

    Add punctuation removal from strings

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,6 @@
 def substrings (phrase, dictionary)
-    phrase = phrase.downcase
-    word_frequency = dictionary.reduce(Hash.new(0)) do |listing, word|
+    phrase = phrase.downcase.gsub(/[,!?.]/, "")
+    word_frequency = dictionary.reduce(Hash.new(0)) do |word_frequency, word|
         if phrase.include?(word)
             word_frequency[word] += 1
         end
@@ -10,4 +10,4 @@ def substrings (phrase, dictionary)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+substrings("b.e,l!o?w", dictionary)
