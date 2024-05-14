b89286f3a28b61e090718fac4b68b9bfd83d9db7
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-14 14:43:36 -0700

    Add substrings function
    This function will output a hash with the counts
    of the words in the dictionary, that are found in
    the phrase, provided by the user.

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,13 @@
 def substrings (phrase, dictionary)
-    # convert phrase to lowercase
-    # iterate through dictionary to see if words are in phrase
-    # if word is in phrase add to hash and one count
-    # return hash 
+    phrase = phrase.downcase
+    word_frequency = dictionary.reduce(Hash.new(0)) do |listing, word|
+        if phrase.include?(word)
+            word_frequency[word] += 1
+        end
+        word_frequency 
+    end
+    puts word_frequency
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
