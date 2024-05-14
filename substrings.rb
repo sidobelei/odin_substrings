411c81622125f3e28dffcfe91f5e1c16b38986ac
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-14 15:16:32 -0700

    Add multi-word argument functionality
    Initially the substrings function could only take
    one word at a time. Now it can take a whole phrase
    and check if any words match an entry in a
    dictionary array.

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,16 @@
 def substrings (phrase, dictionary)
-    phrase = phrase.downcase.gsub(/[,!?.]/, "")
-    word_frequency = dictionary.reduce(Hash.new(0)) do |word_frequency, word|
-        if phrase.include?(word)
-            word_frequency[word] += 1
+    phrase = phrase.downcase.gsub(/[,!?.]/, "").split(" ")
+    word_frequency = dictionary.reduce(Hash.new(0)) do |word_frequency, entry|
+        phrase.each do |word|
+            if word.include?(entry)
+                word_frequency[entry] += 1
+            end
         end
         word_frequency 
     end
     puts word_frequency
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("b.e,l!o?w", dictionary)
+# Uncomment below to get output and change parameters
+#dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+#substrings("Howdy partner, sit down! How's it going?", dictionary)
