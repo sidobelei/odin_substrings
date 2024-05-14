89ada3c21aa0a9fcb8311ad29680d11faf3c8f7a
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-14 14:13:59 -0700

    Add substrings.rb and pseduocode

---
+++ substrings.rb
@@ -0,0 +1,6 @@
+def substrings (phrase, dictionary)
+    # convert phrase to lowercase
+    # iterate through dictionary to see if words are in phrase
+    # if word is in phrase add to hash and one count
+    # return hash 
+end
